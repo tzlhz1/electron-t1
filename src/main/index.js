78efaa848f3ce10bb9093d7cc4b64cd4a01e421e
tzlhz1 <wx_b867bd31502a4596bb1e99b313c41b9f@git.code.tencent.com>
2021-03-04 20:16:44 +0800

    fix:修改窗口初始化，和托盘图标

--- src/main/index.js
+++ src/main/index.js
@@ -1,48 +1,50 @@
-import { app, BrowserWindow } from 'electron'
-
+import console from 'console'
+import { app, BrowserWindow ,Tray} from 'electron' // 引入 app 和 BrowserWindow
+import {options} from './windowList'
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  
 }
-
+console.log('dev---->',`${__static}`)
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
-
+const createSettingWindow = ()=>{
+  
+}
 function createWindow () {
   /**
    * Initial window options
    */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
-  })
+  
+  mainWindow = new BrowserWindow(options) // 创建一个窗口
 
-  mainWindow.loadURL(winURL)
+  mainWindow.loadURL(winURL) // 加载窗口 的 url =>来自renderer进程的页面
 
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+  
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow) // app 准备好的时候创建窗口
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+}) //所有窗口 被关闭触发
 
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
-})
+}) // 当进程处于活动状态时触发
 
 /**
  * Auto Updater
@@ -63,3 +65,8 @@ app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
  */
+
+let tray = null
+app.whenReady().then(() => {
+  tray = new Tray(`${__static}/menubar.png`) // ${__static} 当前static 路径
+})
---
+++ src/main/windowList.js
@@ -0,0 +1,11 @@
+export const options = {
+    height: 563,
+    useContentSize: true,
+    frame:false,
+    fullscreen:false,
+    backgroundColor:'#fff',
+    title:'TlT',
+    width: 500
+}
+
+    
